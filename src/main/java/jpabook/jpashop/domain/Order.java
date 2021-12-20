e9b15c1265c10e50726e71eba19d1a12ca8541d0
jemin <jemin03120111@gmail.com>
2021-12-20 23:58:50 +0900

    상품주문 로직 구현

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -12,6 +14,7 @@ import java.util.List;
 @Table(name = "orders")
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
     @Id
@@ -30,7 +33,7 @@ public class Order {
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
-    private LocalDateTime localDateTime;
+    private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;  // 주문 상태 (order, cancel)
@@ -54,6 +57,61 @@ public class Order {
     {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    // ==== 생성 메서드 ===== //
+    // 복잡한 생성은 생성 메서드가 있으면 좋다
+    // 생성 지점 변경이 있어야 되면 이 메서드만 생성하면 됨! 응집성 향상
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems)
+    {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for(OrderItem orderItem : orderItems)
+        {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    /**
+     * 주문 취소
+     */
+    public void cancel()
+    {
+        if(delivery.getStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송 완료된 상품은 취소가 불가능 합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for(OrderItem orderItem : orderItems)
+        {
+            orderItem.cancel();  // 연관관계 편의 메서드 만들어줬을때 의미 있음
+        }
+
+    }
+
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice()
+    {
+        int totalPrice = 0;
+        for(OrderItem orderItem: orderItems)
+        {
+            totalPrice  += orderItem.getTotalPrice();
+        }
+        return totalPrice;
     }
 
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
@@ -9,6 +11,7 @@ import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -25,4 +28,26 @@ public class OrderItem {
 
     private int orderPrice; // 실제 아이템 가격과 주문 들어간 가격을 다를 수 있다. 할인이 있을수도 있으니!
     private int count; // 주문 수량이 몇개냐?
+
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
+
+    /**
+     * 생성 메서드
+     */
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count)
+    {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
 }
