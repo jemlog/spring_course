edd27a7c25cd5996aa66b5384e9f2bca47e5763f
jemin <jemin03120111@gmail.com>
2022-01-05 22:45:03 +0900

    JPA & QueryDSL 토이 프로젝트 설정

---
+++ src/main/java/study/jpatoyproject/JpaToyprojectApplication.java
@@ -0,0 +1,13 @@
+package study.jpatoyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaToyprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaToyprojectApplication.class, args);
+    }
+
+}
