73ce37f5f52bbf358d3db74d09c3404aa2057125
jemin <jemin03120111@gmail.com>
2022-01-05 18:26:40 +0900

    1 chapter 로그 추적기 코드

---
+++ src/main/java/hello/advanced/app/v1/OrderControllerV1.java
@@ -0,0 +1,35 @@
+package hello.advanced.app.v1;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV1 {
+
+    private final OrderServiceV1 orderService;
+    private final HelloTraceV1 trace;
+
+    @GetMapping("/v1/request")
+    public String request(String itemId) // 클린코드 3장 : 함수는 한가지 작업만 해야한다. request 메서드에 try catch를
+                                         // 걸었다면 catch 구문에서 메서드가 끝나게 하자. 다른 로직 추가 금지!
+    {
+        TraceStatus status = null;
+        try {  // try 블록 안에서만 유효
+               // 클린코드 3장 : try catch 구문 안의 코드는 한줄로 줄이자. 외부로 메서드 추출 해주는게 좋음
+            status = trace.begin("orderController.request()");
+            orderService.orderItem(itemId);  // try catch가 없으면 예외가 그대로 나가버린다.
+            trace.end(status);
+            return "ok";
+        }catch (Exception e)
+        {
+            trace.exception(status,e);  // e를 throw 안해주면 에러가 발생하지 않는다. 정상 흐름으로 변하면 안된다.
+            throw e; // 예외를 꼭 다시 던져줘야 한다.
+        }
+
+
+    }
+}
--- src/main/java/hello/advanced/trace/TraceId.java
+++ src/main/java/hello/advanced/trace/TraceId.java
@@ -1,7 +1,45 @@
 package hello.advanced.trace;
 
+import java.util.UUID;
+
 public class TraceId {
 
     private String id; // 트랜젝션 id
     private int level;  // 깊이
+
+    public TraceId() {
+        this.id = createId();
+        this.level = 0;
+    }
+
+    private TraceId(String id, int level)
+    {
+        this.id = id;
+        this.level = level;
+    }
+
+    private String createId() {
+        return UUID.randomUUID().toString().substring(0,8); // substring => 원하는 길이만큼 자르는 법
+    }
+
+    public TraceId createNextId(){
+        return new TraceId(id,level + 1);
+    }
+
+    private TraceId createPreviousId(){
+        return new TraceId(id,level - 1);
+    }
+
+    public boolean isFirstLevel()
+    {
+        return level == 0;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
 }
